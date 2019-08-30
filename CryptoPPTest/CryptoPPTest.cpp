31a9032f3935f32cd08dc85470908049f97e8028
Dola-Shuvi <16194579+Dola-Shuvi@users.noreply.github.com>
2019-08-30 16:51:46 +0200

    Implement compression via Crypto++ and alternatively via zopfli

--- CryptoPPTest/CryptoPPTest.cpp
+++ CryptoPPTest/CryptoPPTest.cpp
@@ -11,7 +11,14 @@ using namespace std;
 #include "aes.h"
 #include "CryptoPPTest.h"
 #include "sha.h"
+#include "zlib.h"
 using namespace CryptoPP;
+
+#ifdef USEZOPFLI
+#include "zopfli.h"
+#endif // USEZOPFLI
+
+
 
 int main(int argc, char* argv[])
 {
@@ -22,19 +29,40 @@ int main(int argc, char* argv[])
 	memcpy(key, generateSHA256(argv[1]), sizeof(key));
 	memcpy(iv, key, sizeof(iv));
 
-	vector<unsigned char> plain = { 65, 116, 116, 97, 99, 107, 32, 97, 116, 32, 100, 97, 119, 110, 33 };
+	//vector<unsigned char> plain = { 65, 116, 116, 97, 99, 107, 32, 97, 116, 32, 100, 97, 119, 110, 33, 65, 116, 116, 97, 99, 107, 32, 97, 116, 32, 100, 97, 119, 110, 33, 65, 116, 116, 97, 99, 107, 32, 97, 116, 32, 100, 97, 119, 110, 33 };
+	
+	vector<unsigned char> plain = readAllBytes("lorem.txt");
 
-	std::copy(plain.begin(), plain.end(), std::ostream_iterator<char>(std::cout, ""));
+	//std::copy(plain.begin(), plain.end(), std::ostream_iterator<char>(std::cout, ""));
+	cout << plain.size() << endl;
+	cout << endl;
+
+#ifdef USEZOPFLI
+	vector<byte> compressed = zopfliCompress(plain);
+#else
+	vector<byte> compressed = zlibCompress(plain);
+#endif	
+
+	//std::copy(compressed.begin(), compressed.end(), std::ostream_iterator<char>(std::cout, ""));
+	cout << compressed.size() << endl;
 	cout << endl;
 
 	/////////////////////////////////////////////////////////////
-	vector<unsigned char> encrypted = Encrypt(key, iv, plain);
-	std::copy(encrypted.begin(), encrypted.end(), std::ostream_iterator<char>(std::cout, ""));
+	vector<unsigned char> encrypted = Encrypt(key, iv, compressed);
+	//std::copy(encrypted.begin(), encrypted.end(), std::ostream_iterator<char>(std::cout, ""));
+	cout << encrypted.size() << endl;
 	cout << endl;
 	vector<unsigned char> decrypted = Decrypt(key, iv, encrypted);
-	std::copy(decrypted.begin(), decrypted.end(), std::ostream_iterator<char>(std::cout, ""));
+	//std::copy(decrypted.begin(), decrypted.end(), std::ostream_iterator<char>(std::cout, ""));
+	cout << decrypted.size() << endl;
 	cout << endl;
 	/////////////////////////////////////////////////////////////
+
+	vector<byte> decompressed = zlibDecompress(compressed);
+
+	//std::copy(decompressed.begin(), decompressed.end(), std::ostream_iterator<char>(std::cout, ""));
+	cout << decompressed.size() << endl;
+	cout << endl;
 
 	return 0;
 }
@@ -56,7 +84,7 @@ vector<unsigned char> Encrypt(byte key[], byte iv[], vector<unsigned char> data)
 	cipher.resize(data.size() + AES::BLOCKSIZE);
 	ArraySink cs(&cipher[0], cipher.size());
 
-	ArraySource(data.data(), data.size(), true,
+	(void)ArraySource(data.data(), data.size(), true,
 		new StreamTransformationFilter(enc, new Redirector(cs), StreamTransformationFilter::PKCS_PADDING));
 
 	// Set cipher text length now that its known
@@ -82,8 +110,8 @@ vector<unsigned char> Decrypt(byte key[], byte iv[], vector<unsigned char> data)
 	recover.resize(data.size());
 	ArraySink rs(&recover[0], recover.size());
 
-	ArraySource(data.data(), data.size(), true,
-		new StreamTransformationFilter(dec, new Redirector(rs), StreamTransformationFilter::PKCS_PADDING));	
+	(void)ArraySource(data.data(), data.size(), true,
+		new StreamTransformationFilter(dec, new Redirector(rs), StreamTransformationFilter::PKCS_PADDING));
 
 	// Set recovered text length now that its known
 	recover.resize(rs.TotalPutLength());
@@ -94,10 +122,97 @@ vector<unsigned char> Decrypt(byte key[], byte iv[], vector<unsigned char> data)
 byte* generateSHA256(string data)
 {
 	byte const* pbData = (byte*)data.data();
-	unsigned int nDataLen = data.size();
-	byte abDigest[SHA256::DIGESTSIZE];
+	size_t nDataLen = data.size();
+	byte* abDigest = new byte[SHA256::DIGESTSIZE];
 
 	SHA256().CalculateDigest(abDigest, pbData, nDataLen);
 
 	return abDigest;
+}
+
+vector<byte> zlibCompress(vector<byte> input) {
+	ZlibCompressor zipper;
+	zipper.Put((byte*)input.data(), input.size());
+	zipper.MessageEnd();
+
+	word64 avail = zipper.MaxRetrievable();
+	if (avail)
+	{
+		vector<byte> compressed;
+		compressed.resize(avail);
+
+		zipper.Get(&compressed[0], compressed.size());
+		return compressed;
+	}
+}
+
+vector<byte> zlibDecompress(vector<byte> input) {
+	ZlibDecompressor zipper;
+	zipper.Put((byte*)input.data(), input.size());
+	zipper.MessageEnd();
+
+	word64 avail = zipper.MaxRetrievable();
+	vector<byte> decompressed;
+	decompressed.resize(avail);
+
+	zipper.Get(&decompressed[0], decompressed.size());
+
+	return decompressed;
+}
+
+#ifdef USEZOPFLI
+vector<byte> zopfliCompress(vector<byte> input) {
+	ZopfliOptions options;
+	ZopfliInitOptions(&options);
+
+	if (input.size() > 10000000) {
+		options.blocksplittingmax = 32;
+		options.numiterations = 8;
+	}
+	else if(input.size() > 1000000) {
+		options.blocksplittingmax = 32;
+		options.numiterations = 16;
+	}
+	else {
+		options.blocksplittingmax = 32;
+		options.numiterations = 32;
+	}
+
+	
+
+	size_t size = 0;
+	unsigned char* temp;
+
+	ZopfliCompress(&options, ZOPFLI_FORMAT_ZLIB, input.data(), input.size(), &temp, &size);
+
+	vector<unsigned char> output(temp, temp + size);
+	output.shrink_to_fit();
+
+	return output;
+
+}
+#endif // USEZOPFLI;
+
+
+
+vector<unsigned char> readAllBytes(string fileName) {
+	//Open file
+	ifstream infile(fileName, ios::in | ios::binary);
+	vector<unsigned char> buffer;
+
+	//Get length of file
+	infile.seekg(0, infile.end);
+	size_t length = infile.tellg();
+	infile.seekg(0, infile.beg);
+
+	//Read file
+	if (length > 0) {
+		buffer.resize(length);
+		infile.read((char*)& buffer[0], length);
+	}
+
+	//Close instream
+	infile.close();
+
+	return buffer;
 }
--- CryptoPPTest/CryptoPPTest.h
+++ CryptoPPTest/CryptoPPTest.h
@@ -6,3 +6,13 @@ vector<unsigned char> Encrypt(byte key[], byte iv[], vector<unsigned char> data)
 vector<unsigned char> Decrypt(byte key[], byte iv[], vector<unsigned char> data);
 
 byte* generateSHA256(string data);
+
+vector<byte> zlibCompress(vector<byte> input);
+
+vector<byte> zlibDecompress(vector<byte> input);
+
+#ifdef USEZOPFLI
+vector<byte> zopfliCompress(vector<byte> input);
+#endif // USEZOPFLI
+
+vector<unsigned char> readAllBytes(string fileName);
